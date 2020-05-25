31967423fa9198854a401c35d2c3f645e0b2617c
Wes Chow <wgesh8@gmail.com>
2020-05-25 13:05:16 -0700

    Add classes, start interface

---
+++ src/Material.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wes
+ */
+public class Material {
+    
+    private String Type;
+    // other variables
+    
+    public Material(String Type) {
+        this.Type = Type;
+    }
+}
